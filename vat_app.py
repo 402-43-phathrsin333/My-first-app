0f641ddc2318892cc4257812fae2df95df9d9142
402-43-phathrsin333 <phathrsin1435@gmail.com>
2026-08-05 15:19:09 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -4,6 +4,6 @@ price = st.number_input("กรอกราคาสินค้า (บาท):
 vat = price * 0.07
 net_price = price - vat
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
-st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+st.header(f"• ราคาสุทธิ:{net_price:.2f} บาท")
 st.divider()
 st.write("ภัทรสิน ศิลธรรม เลขที่ 43 ม.4/2")
