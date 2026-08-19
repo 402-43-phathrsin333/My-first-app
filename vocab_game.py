b75bde7b1a34acfc8be2c27923f5f418cd93a806
402-43-phathrsin333 <phathrsin1435@gmail.com>
2026-08-19 15:30:49 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -38,7 +38,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
         st.success("✅ ข้อ 1: ถูกต้อง")
         score += 1
     else:
-        st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
+        st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
 
     # ตรวจข้อ 2
     if u_ans2 == "fish":
